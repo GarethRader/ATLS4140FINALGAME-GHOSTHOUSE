4b6f27bf83dcf6a6be5804e28b2eceb9f0bb9923
GarethRader <garethm.rader@gmail.com>
2020-11-09 00:58:11 -0700

    movement system prototype

---
+++ Assets/Scripts/TopDownCharacterMover.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputHandler))]
+public class TopDownCharacterMover : MonoBehaviour
+{
+    private InputHandler _input;
+
+    // jumping
+    private Vector3 jump;
+    [SerializeField]
+    private float jumpForce;
+    private bool isGrounded = true;
+    [SerializeField]
+    private Rigidbody rb;
+
+
+    // movement
+    [SerializeField]
+    private bool RotateTowardMouse;
+
+    [SerializeField]
+    private float MovementSpeed;
+    [SerializeField]
+    private float RotationSpeed;
+
+    [SerializeField]
+    private Camera Camera;
+
+    private void Awake()
+    {
+        _input = GetComponent<InputHandler>();
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 10.0f,0.0f); // change this to change jump height
+    }
+
+    
+    private void Update()
+    {
+        HandleMovement();
+        HandleJump();
+    }
+
+    //JUMP FUNCTIONS
+    private void OnCollisionStay(){
+        isGrounded = true;
+    }
+    private void HandleJump(){
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    //MOVEMENT FUNCTIONS
+    private void HandleMovement(){
+        var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
+        var movementVector = MoveTowardTarget(targetVector);
+
+        if (!RotateTowardMouse)
+        {
+            RotateTowardMovementVector(movementVector);
+        }
+        if (RotateTowardMouse)
+        {
+            RotateFromMouseVector();
+        }
+    }
+    private void RotateFromMouseVector()
+    {
+        Ray ray = Camera.ScreenPointToRay(_input.MousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f))
+        {
+            var target = hitInfo.point;
+            target.y = transform.position.y;
+            transform.LookAt(target);
+        }
+    }
+
+    private Vector3 MoveTowardTarget(Vector3 targetVector)
+    {
+        var speed = MovementSpeed * Time.deltaTime;
+
+        targetVector = Quaternion.Euler(0, Camera.gameObject.transform.rotation.eulerAngles.y, 0) * targetVector;
+        var targetPosition = transform.position + targetVector * speed;
+        transform.position = targetPosition;
+        return targetVector;
+    }
+
+    private void RotateTowardMovementVector(Vector3 movementDirection)
+    {
+        if(movementDirection.magnitude == 0) { return; }
+        var rotation = Quaternion.LookRotation(movementDirection);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, RotationSpeed);
+    }
+}
